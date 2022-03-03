7d2b2556862e08b052a3f5441ce2fc21584271a2
Sudi Simba <sudisimbadav@gmail.com>
2022-03-03 05:56:24 +0200

    Complete the movie models and its interfaces declarations

--- MovieMVC/GlobalUsings.cs
+++ MovieMVC/GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using MovieMVC.Enums;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
+global using MovieMVC.Models.Database;
+global using MovieMVC.Models.Tmdb;
---
+++ MovieMVC/Services/Interfaces/IImageService.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieMVC.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodingImageAsync(IFormFile poster);
+        Task<byte[]> EncodingImageuUrlAsync(string ImageUrl);
+        string DecodeImage(byte[] poster, string contentType);
+    }
+}
