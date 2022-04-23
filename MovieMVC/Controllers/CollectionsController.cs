efa4f0795b6da740485fa6def14afb4cf4ec8a03
Sudi Simba <sudisimbadav@gmail.com>
2022-04-23 12:13:56 +0200

    Collections Controller end points completed

--- MovieMVC/Controllers/CollectionsController.cs
+++ MovieMVC/Controllers/CollectionsController.cs
@@ -2,9 +2,126 @@
 {
     public class CollectionsController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly AppSettings _appSettings;
+
+        public CollectionsController(IOptions<AppSettings> appSettings, ApplicationDbContext context)
         {
-            return View();
+            _appSettings = appSettings.Value;
+            _context = context;
         }
+
+        // GET: Collections
+        public async Task<IActionResult> Index()
+        {
+            // Get all collections except the default collections one
+            var defaultCollectionName = _appSettings.MovieMVCSettings.DefaultCollection.Name;
+            var collections = await _context.Collection.Where(c => c.Name != defaultCollectionName).ToListAsync();
+
+            return View(collections);
+        }
+
+        // POST: Collections/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Collection collection)
+        {
+            _context.Add(collection);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "MovieCollections", new { id = collection.Id });
+        }
+
+        // GET: Collections/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var collection = await _context.Collection.FindAsync(id);
+            if (collection == null)
+            {
+                return NotFound();
+            }
+            return View(collection);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Collection collection)
+        {
+            if (id != collection.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (collection.Name == _appSettings.MovieMVCSettings.DefaultCollection.Name)
+                    {
+                        return RedirectToAction("Index", "Collections");
+                    }
+
+                    _context.Update(collection);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CollectionExists(collection.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(collection);
+        }
+
+        // GET: Collections/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var collection = await _context.Collection
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (collection == null)
+            {
+                return NotFound();
+            }
+
+            if (collection.Name == _appSettings.MovieMVCSettings.DefaultCollection.Name)
+            {
+                return RedirectToAction("Index", "Collections");
+            }
+
+            return View(collection);
+        }
+
+        // POST: Collections/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var collection = await _context.Collection.FindAsync(id);
+            _context.Collection.Remove(collection);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "MovieCollections");
+        }
+
+        private bool CollectionExists(int id)
+        {
+            return _context.Collection.Any(e => e.Id == id);
+        }
+
     }
 }
